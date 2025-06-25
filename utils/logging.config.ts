b11e3df6062e504444acb112aee1895a20cc320f
bob@babbageproject.com <bob@babbageproject.com>
2025-06-25 23:02:01 +0100

    Latest logging utils that use a config file to switch on/off

---
+++ utils/logging.config.ts
@@ -0,0 +1,10 @@
+// Default logging state for all files
+const defaultLogging = false
+
+// Specific file logging overrides
+const loggingConfig: { [file: string]: boolean } = {
+  default: defaultLogging,
+  'context/WalletContext.tsx': true
+}
+
+export default loggingConfig
