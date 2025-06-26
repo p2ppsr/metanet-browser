10839b5a7edfc7121c54c92632c94d80feceb8d4
bob@babbageproject.com <bob@babbageproject.com>
2025-06-26 21:05:32 +0100

    Added right swipe delete tab, identical to current left swipe delete tab

--- app/browser.tsx
+++ app/browser.tsx
@@ -1578,14 +1578,42 @@ const TabsViewBase = ({
       </Animated.View>
     )
   }
+  const renderLeftActions = (
+    progress: Animated.AnimatedInterpolation<number>,
+    dragX: Animated.AnimatedInterpolation<number>
+  ) => {
+    const trans: Animated.AnimatedInterpolation<number> = dragX.interpolate({
+      inputRange: [0, 101],
+      outputRange: [1, 0],
+      extrapolate: 'clamp'
+    })
+    return (
+      <Animated.View
+        style={[
+          styles.swipeDelete,
+        ]}
+      >
+      </Animated.View>
+    )
+  }
 
   return (
     <Swipeable
       renderRightActions={renderRightActions}
+      renderLeftActions={renderLeftActions}
       onSwipeableRightOpen={() => tabStore.closeTab(item.id)}
+      onSwipeableLeftOpen={() => tabStore.closeTab(item.id)}
       friction={2}
       rightThreshold={40}
+      leftThreshold={40}
     >
+  ) => {
+    const trans: Animated.AnimatedInterpolation<number> = dragX.interpolate({
+      inputRange: [-101, 0],
+      outputRange: [0, 1],
+      extrapolate: 'clamp'
+    })
+    return (
       <Pressable
         style={[
           styles.tabPreview,
